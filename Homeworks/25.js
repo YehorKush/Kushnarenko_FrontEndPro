ba41c5e5237b5a4db5ac35b436f45a674df49322
YehorKush <kushnarenkoegor@gmail.com>
2023-05-04 11:48:31 +0100

    25

--- Homeworks/25.js
+++ Homeworks/25.js
@@ -12,5 +12,44 @@
 // При введенні znak потрібно перевірити коректність введення на можливі
 // математичні дії.
 
-// p = new SuperMath();
-// p.check(obj); // --> no p.input() -> 3 prompt -> рахує
+class SuperMath {
+  input() {
+    if (confirm("Ввести новi даннi?")) {
+      let x = prompt("Введiть новий X", 4);
+      let y = prompt("Введiть новий Y", 2);
+      let znak = prompt("Введiть новий X", "/");
+      return { x: x, y: y, znak: znak };
+    } else return 0;
+  }
+}
+
+let math = new SuperMath();
+
+math.check = function (obj) {
+  if (confirm(`Зробити дію '${obj.znak}' з числами ${obj.x} і ${obj.y}?`)) {
+    switch (obj.znak) {
+      case "+":
+        console.log(obj.x + obj.y);
+        break;
+      case "-":
+        console.log(obj.x - obj.y);
+        break;
+      case "/":
+        console.log(obj.x / obj.y);
+        break;
+      case "*":
+        console.log(obj.x * obj.y);
+        break;
+      case "%":
+        console.log(obj.x % obj.y);
+        break;
+    }
+  } else {
+    let newObj = math.input();
+    newObj == 0 ? console.log("Вiдмова") : math.check(newObj);
+  }
+};
+
+let first = { x: 12, y: 3, znak: "/" }; 
+
+math.check(first);
